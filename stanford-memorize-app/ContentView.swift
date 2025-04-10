108d5b87f35e39d008dbde51b6f16a5bd5be982f
Mo Bo <mochobocho@outlook.com>
2025-04-10 20:42:16 +0300

    no other manupulation in a vew exept vars , ifs and lists

--- stanford-memorize-app/ContentView.swift
+++ stanford-memorize-app/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-var body: some View {
+    var body: some View {
         HStack {
             CardView(isFaceUp: true)
             CardView(isFaceUp: false)
@@ -17,18 +17,23 @@ var body: some View {
         }
         .foregroundColor(.orange)
         .padding()
+    }
 }
 
 struct CardView: View {
     var isFaceUp: Bool = false// every value in a struct must have a value when the struct is created
     var body: some View {
         ZStack { //trailing closure syntax, zstack is a struc behaves like a view
+            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            // we cannot do anythig else exept vars , ifs and lists
+            var x: Int = 1
+            x = x + 1
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👀").font(.largeTitle )
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill() //default behavior is fill that is why we don't need to add fill
             }
         }
     }
